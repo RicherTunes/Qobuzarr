8a24b32a013c44c5c633a2c38803fd5f0f608a1e
RicherTunes <xfear26@hotmail.com>
2025-09-02 16:20:02 -0400

    Build against Lidarr plugin branch host: remove stable downloader, build ext/Lidarr-source; use string Protocol + IDownloadProtocol everywhere; update references

--- src/Download/QobuzarrDownloadProtocol.cs
+++ src/Download/QobuzarrDownloadProtocol.cs
@@ -1,14 +1,12 @@
-#if PLUGIN_PROTOCOL
 using NzbDrone.Core.Indexers;
 
 namespace Lidarr.Plugin.Qobuzarr.Download
 {
     /// <summary>
     /// Download protocol marker class for Qobuzarr streaming service
-    /// Compiled only when building against Lidarr plugin branch (string Protocol).
+    /// Compiled against Lidarr plugin branch (string Protocol).
     /// </summary>
     public class QobuzarrDownloadProtocol : IDownloadProtocol
     {
     }
 }
-#endif
